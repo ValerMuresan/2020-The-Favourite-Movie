30716839805dd842bc47f4d75aa5fe6ce92e4b90
Valer-Muresan <muresanvaler86@yahoo.com>
2020-02-13 20:18:38 +0200

    Updates

---
+++ precache-manifest.03212b29f90df042d755dccee37159c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "262d66bf2e6c72c955c203b8374a3ceb",
+    "url": "/2020-The-Favourite-Movie/index.html"
+  },
+  {
+    "revision": "0279d3688bc72dd63c47",
+    "url": "/2020-The-Favourite-Movie/static/css/main.fbc98188.chunk.css"
+  },
+  {
+    "revision": "b41b19c6e1924b4fd90b",
+    "url": "/2020-The-Favourite-Movie/static/js/2.21fa911d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/2020-The-Favourite-Movie/static/js/2.21fa911d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0279d3688bc72dd63c47",
+    "url": "/2020-The-Favourite-Movie/static/js/main.78a76634.chunk.js"
+  },
+  {
+    "revision": "707653c7524fed78a770",
+    "url": "/2020-The-Favourite-Movie/static/js/runtime-main.6205e761.js"
+  }
+]);
